a1afb0b29ce66b154d9d4a8555ece894777dbc8b
no-no <nono.uichi@gmail.com>
2013-03-10 23:00:25 +0900

    修正

--- src/com/example/bijintimer/MainActivity.java
+++ src/com/example/bijintimer/MainActivity.java
@@ -2,20 +2,32 @@ package com.example.bijintimer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    @Override
-      protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	Button start;
+	MyCountdownTimer myCountdownTimer;
 
-        MyCountdownTimer myCountdownTimer = new MyCountdownTimer(100000,1000, (TextView)findViewById(R.id.count_text));
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		start = (Button) findViewById(R.id.startButton);
 
-        myCountdownTimer.start();
+		this.myCountdownTimer = new MyCountdownTimer(180000, 1000,
+				(TextView) findViewById(R.id.count_text));
 
+		start.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// カウントダウンスタート
+				myCountdownTimer.start();
+			}
+		});
+	}
 
-    }
 }
--- src/com/example/bijintimer/MyCountdownTimer.java
+++ src/com/example/bijintimer/MyCountdownTimer.java
@@ -7,18 +7,26 @@ public class MyCountdownTimer extends CountDownTimer {
 
 	TextView timerText;
 
-    public MyCountdownTimer(long millisInFuture, long countDownInterval, TextView timerText) {
+	public MyCountdownTimer(long millisInFuture, long countDownInterval,
+			TextView timerText) {
 		super(millisInFuture, countDownInterval);
 		this.timerText = timerText;
 	}
 
-    // カウントダウン処理
-    public void onTick(long millisUntilFinished){
-    	this.timerText.setText(""+millisUntilFinished/1000);
-    }
-    // カウントが0になった時の処理
-    public void onFinish(){
-    	this.timerText.setText("タイムオーバー");
-    }
+	// カウントダウン処理
+	public void onTick(long millisUntilFinished) {
+
+		long second = millisUntilFinished / 1000;
+
+		long minutes = second / 60;
+		long seconds = second % 60;
+
+		this.timerText.setText(minutes + "：" + seconds);
+	}
+
+	// カウントが0になった時の処理
+	public void onFinish() {
+		this.timerText.setText("終了");
+	}
 
 }
